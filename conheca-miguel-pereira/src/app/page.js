4c1efe0e96d2c0beafc48890b513dda2a6d970fa
Renan Farias <renan.mafarias@gmail.com>
2024-11-30 12:13:10 -0300

    Criação do projeto Next.js

---
+++ conheca-miguel-pereira/src/app/page.js
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <article>
+          <h1>Conheça Miguel Pereira</h1>
+          <div className="detail"></div>
+          <p>Miguel Pereira é um município brasileiro do estado do Rio de Janeiro, Região Sudeste do País. Localizado no Centro-Sul Fluminense, é uma estância climática situada a 618 metros acima do nível do mar.</p>
+          <p>Até os anos 1700, a região era habitada pelos índios coroados (puris). Nesse século, os índios passaram a ser aldeados em missões religiosas.</p>
+          <p>A evolução histórica de Miguel Pereira acha-se ligada à de Vassouras e de Paty do Alferes, e à expansão da cultura cafeeira no vale fluminense do Rio Paraíba do Sul.</p>
+          <p>A ocupação de origem europeia da área de Miguel Pereira teve origem nas primeiras explorações de descendentes de europeus que visavam a transpor a Serra do Mar. Tais explorações prosseguiram com a abertura feita por Garcia Rodrigues Paes do Caminho Novo do Tinguá, entre o Rio de Janeiro e as Minas Gerais.</p>
+          <p>Os tropeiros que subiam o Rio das Mortes em direção a Sacra Família do Tinguá (atualmente, distrito do município de Engenheiro Paulo de Frontin), fixaram ponto de passagem em pequena várzea. Inicialmente, o local ficou conhecido como Barreiros ou Tejuco, pois, aí, se atolavam as tropas de burros que percorriam o Caminho Novo. Depois, passou a se chamar Estiva, nome de uma trama de bambu que os tropeiros usavam para colocar no caminhos dos burros para, assim, vencer a lama em sua jornada.</p>
+          <p>Algumas pequenas lavouras surgiram na região durante o século XVIII. Estas produziam açúcar ou, mais frequentemente, gêneros alimentícios para consumo no Rio de Janeiro. Em 1770, foi fundada a Fazenda da Piedade de Vera Cruz, que se tornaria importante como produtora de café na região. As terras do atual município de Miguel Pereira eram, então, subordinadas administrativa e religiosamente à Freguesia de Nossa Senhora da Conceição do Alferes, atual Paty do Alferes.</p>
+        </article>
+      </main>
+      <footer>
+        <p>Desenvolvido por Renan Farias</p>
+      </footer>
+    </>
+  );
+}
